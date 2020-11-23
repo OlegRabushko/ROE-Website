ce1aba8a13bcb8d80b1f4576af23c3cb62363925
Oleg <oleg.rabyshko@mail.ru>
2020-11-23 14:58:58 +0300

    Updates

---
+++ precache-manifest.dcd67b777c60ac2cd8fb5f43f159e0ba.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514e71ee5593d16d41033c742b71601d",
+    "url": "/ROE-Website/index.html"
+  },
+  {
+    "revision": "9b3ce46f5787e1d58c9e",
+    "url": "/ROE-Website/static/css/3.077d4451.chunk.css"
+  },
+  {
+    "revision": "f4fdf34c123b4e20e751",
+    "url": "/ROE-Website/static/css/4.29e41d80.chunk.css"
+  },
+  {
+    "revision": "c4dbd0ca6f0de6328031",
+    "url": "/ROE-Website/static/css/main.d853e846.chunk.css"
+  },
+  {
+    "revision": "6245263b953817e2eb58",
+    "url": "/ROE-Website/static/js/2.8fb8510d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ROE-Website/static/js/2.8fb8510d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3ce46f5787e1d58c9e",
+    "url": "/ROE-Website/static/js/3.28d5cfc5.chunk.js"
+  },
+  {
+    "revision": "f4fdf34c123b4e20e751",
+    "url": "/ROE-Website/static/js/4.edddecb3.chunk.js"
+  },
+  {
+    "revision": "c4dbd0ca6f0de6328031",
+    "url": "/ROE-Website/static/js/main.7e499694.chunk.js"
+  },
+  {
+    "revision": "5560558977ea361db6ed",
+    "url": "/ROE-Website/static/js/runtime-main.5b919e98.js"
+  },
+  {
+    "revision": "f6857361926a0341f67d0925264dd57c",
+    "url": "/ROE-Website/static/media/img1.f6857361.png"
+  },
+  {
+    "revision": "92a351cf7c39d73f5daa80e888a46c1b",
+    "url": "/ROE-Website/static/media/oboik.ru_7114.92a351cf.jpg"
+  },
+  {
+    "revision": "2b02159fee58d573c079ad5212d56b63",
+    "url": "/ROE-Website/static/media/preloader.2b02159f.gif"
+  },
+  {
+    "revision": "6a6f6ed74fb8cdacb849a39fd0a90731",
+    "url": "/ROE-Website/static/media/user.6a6f6ed7.png"
+  }
+]);
